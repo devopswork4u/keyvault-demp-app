b8635cf60cda8ce2b67012e7b6a913151e77654d
Rahul Sharma <kaushikrahul08@gmail.com>
2025-09-28 17:29:18 -0400

    added file

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from azure.keyvault.secrets import SecretClient
 import os
 import traceback
 
+from flask import render_template
+
+
+
 app = Flask(__name__)
 
 # Read environment variables
@@ -29,14 +33,10 @@ except Exception as e:
     traceback.print_exc()
     secret_value = f"ERROR: {str(e)}"
 
+
 @app.route("/")
 def index():
-    return f"""
-    <h1>Azure Key Vault Secret Demo</h1>
-    <p><strong>Vault:</strong> {KEY_VAULT_URL}</p>
-    <p><strong>Secret Name:</strong> {SECRET_NAME}</p>
-    <p><strong>Secret Value:</strong> {secret_value}</p>
-    """
+    return render_template("index.html", secret_value=secret_value)
 
 if __name__ == "__main__":
     app.run()
